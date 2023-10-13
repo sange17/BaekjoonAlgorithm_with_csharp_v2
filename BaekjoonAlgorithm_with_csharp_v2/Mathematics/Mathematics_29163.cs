c5d558c4070c31530ce1c5fe98f0d2174dd546e1
sange17 <sange17@naver.com>
2023-10-13 21:35:01 +0900

    Add mathematics_29163.cs

---
+++ BaekjoonAlgorithm_with_csharp_v2/Mathematics/Mathematics_29163.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Mathematics
+{
+    class Mathematics_29163
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            string[] line = Console.ReadLine().Split(' ');
+
+            int count1 = 0;
+            int count2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if(Int32.Parse(line[i]) % 2 == 0)
+                {
+                    count1++;
+                }
+                else
+                {
+                    count2++;
+                }
+            }
+
+            if(count1 > count2)
+            {
+                Console.WriteLine("Happy");
+            }
+            else
+            {
+                Console.WriteLine("Sad");
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using BaekjoonAlgorithm_with_csharp_v2.Mathematics;
 
 namespace BaekjoonAlgorithm_with_csharp_v2
 {
@@ -10,6 +7,9 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
+            Mathematics_29163 func = new Mathematics_29163();
+
+            func.solve();
         }
     }
 }
