38bd8b08dfe8eb9ac1ac2458a428296815e47cfe
sange17 <sange17@naver.com>
2023-10-18 17:14:21 +0900

    Add mathematics_30328.cs

---
+++ BaekjoonAlgorithm_with_csharp_v2/Mathematics/Mathematics_30328.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Mathematics
+{
+    class Mathematics_30328
+    {
+        public void solve()
+        {
+            Console.WriteLine(Int32.Parse(Console.ReadLine()) * 4000);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -9,7 +9,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_30224 func = new Mathematics_30224();
+            Mathematics_30328 func = new Mathematics_30328();
 
             func.solve();
         }
