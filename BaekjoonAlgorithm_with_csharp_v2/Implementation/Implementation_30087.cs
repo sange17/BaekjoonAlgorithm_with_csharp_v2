5a43d253451c5fe8564ff2794952b04fa16773e4
sange17 <sange17@naver.com>
2023-10-21 14:09:28 +0900

    Add implementation_30087.cs

---
+++ BaekjoonAlgorithm_with_csharp_v2/Implementation/Implementation_30087.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Implementation
+{
+    class Implementation_30087
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            string line = string.Empty;
+            for(int i = 0; i < N; i++)
+            {
+                line = Console.ReadLine();
+
+                if (line.Equals("Algorithm")) Console.WriteLine("204");
+                if (line.Equals("DataAnalysis")) Console.WriteLine("207");
+                if (line.Equals("ArtificialIntelligence")) Console.WriteLine("302");
+                if (line.Equals("CyberSecurity")) Console.WriteLine("B101");
+                if (line.Equals("Network")) Console.WriteLine("303");
+                if (line.Equals("Startup")) Console.WriteLine("501");
+                if (line.Equals("TestStrategy")) Console.WriteLine("105");
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -9,7 +9,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_30214 func = new Mathematics_30214();
+            Implementation_30087 func = new Implementation_30087();
 
             func.solve();
         }
