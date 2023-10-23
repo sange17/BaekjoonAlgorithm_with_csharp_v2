c3a156ade9d119e631002bfa1529e5c3816858e9
sange17 <sange17@naver.com>
2023-10-23 18:02:09 +0900

    Add implementation_28490.cs

---
+++ BaekjoonAlgorithm_with_csharp_v2/Implementation/Implementation_28490.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Implementation
+{
+    class Implementation_28490
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            int max = 0;
+
+            string line = string.Empty;
+            int a = 0;
+            int b = 0;
+            for(int i = 0; i < N; i++)
+            {
+                line = Console.ReadLine();
+
+                a = Int32.Parse(line.Split(' ')[0]);
+                b = Int32.Parse(line.Split(' ')[1]);
+
+                if(a * b > max)
+                {
+                    max = a * b;
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -9,7 +9,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_28519 func = new Mathematics_28519();
+            Implementation_28490 func = new Implementation_28490();
 
             func.solve();
         }
