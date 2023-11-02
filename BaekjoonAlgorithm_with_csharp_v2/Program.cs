aa9089a0a5752aac2eaee3082e2ebb920735d422
sange17 <sange17@naver.com>
2023-11-02 18:04:02 +0900

    Add strings_29766.cs

--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -3,6 +3,8 @@ using BaekjoonAlgorithm_with_csharp_v2.Mathematics;
 using BaekjoonAlgorithm_with_csharp_v2.Implementation;
 using BaekjoonAlgorithm_with_csharp_v2.Graphs;
 using BaekjoonAlgorithm_with_csharp_v2.DP;
+using BaekjoonAlgorithm_with_csharp_v2.Strings;
+
 
 namespace BaekjoonAlgorithm_with_csharp_v2
 {
@@ -10,7 +12,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_27918 func = new Implementation_27918();
+            Strings_29766 func = new Strings_29766();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_v2/Strings/Strings_29766.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Strings
+{
+    class Strings_29766
+    {
+        public void solve()
+        {
+            string line = Console.ReadLine();
+            int lineLength = line.Length;
+
+            int count = 0;
+            for(int i = 0; i < lineLength - 3; i++)
+            {
+                if(line[i] == 'D' && line[i + 1] == 'K' && line[i + 2] == 'S' && line[i + 3] == 'H')
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
