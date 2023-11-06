0cb21fc4aed29f1d0124287068cd3a51ae9436e5
sange17 <sange17@naver.com>
2023-11-06 20:46:21 +0900

    Add graphs_12851.cs

---
+++ BaekjoonAlgorithm_with_csharp_v2/Graphs/Graphs_12851.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaekjoonAlgorithm_with_csharp_v2.Graphs
+{
+    class Graphs_12851
+    {
+        static int N = 0;
+        static int K = 0;
+        static int[] time = new int[100001];
+        static int minTime = 987654321;
+        static int count = 0;
+
+        static void bfs()
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(N);
+            time[N] = 1;
+
+            int now = 0;
+            int next = 0;
+            while (queue.Count != 0)
+            {
+                now = queue.Dequeue();
+
+                if (minTime < time[now]) return;
+
+                for(int i = 0; i < 3; i++)
+                {
+                    if (i == 0) next = now + 1;
+                    else if (i == 1) next = now - 1;
+                    else next = now * 2;
+
+                    if (next < 0 || next > 100000) continue;
+
+                    if(next == K)
+                    {
+                        minTime = time[now];
+                        count++;
+                    }
+
+                    if(time[next] == 0 || time[next] == time[now] + 1)
+                    {
+                        queue.Enqueue(next);
+                        time[next] = time[now] + 1;
+                    }
+                }
+            }
+        }
+
+        public void solve()
+        {
+            string line = Console.ReadLine();
+
+            N = Int32.Parse(line.Split(' ')[0]);
+            K = Int32.Parse(line.Split(' ')[1]);
+
+            if(N >= K)
+            {
+                Console.WriteLine((N - K) + "\n1");
+                return;
+            }
+
+            bfs();
+
+            Console.WriteLine(minTime + "\n" + count);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_v2/Program.cs
+++ BaekjoonAlgorithm_with_csharp_v2/Program.cs
@@ -12,7 +12,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_28453 func = new Implementation_28453();
+            Graphs_12851 func = new Graphs_12851();
 
             func.solve();
         }
