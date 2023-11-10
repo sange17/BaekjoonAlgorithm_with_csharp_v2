a3ddb93830c7404eaed4461eee8e24a471f42d2f
sange17 <sange17@naver.com>
2023-11-10 21:04:50 +0900

    Add mathematics_28248.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_28248.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_28248
+    {
+        public void solve()
+        {
+            int P = Int32.Parse(Console.ReadLine());
+            int C = Int32.Parse(Console.ReadLine());
+
+            if(P > C)
+            {
+                Console.WriteLine(P * 50 - C * 10 + 500);
+            }
+            else
+            {
+                Console.WriteLine(P * 50 - C * 10);
+            }
+        }
+    }
+}
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics;
+
+namespace BaekjoonAlgorithm_with_csharp_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mathematics_28248 func = new Mathematics_28248();
+
+            func.solve();
+        }
+    }
+}
