44f45c6f245ca986877bfbd23001c273fdc3c0fc
sange17 <sange17@naver.com>
2023-11-22 23:04:31 +0900

    Add Implementation_30676.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_30676.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_v2.implementation
+{
+    class Implementation_30676
+    {
+        static void Main(String[] args)
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            if (N >= 620 && N <= 780)
+            {
+                Console.WriteLine("Red");
+            }
+            else if (N >= 590 && N < 620)
+            {
+                Console.WriteLine("Orange");
+            }
+            else if (N >= 570 && N < 590)
+            {
+                Console.Write("Yellow");
+            }
+            else if (N >= 495 && N < 570)
+            {
+                Console.WriteLine("Green");
+            }
+            else if (N >= 450 && N < 495)
+            {
+                Console.WriteLine("Blue");
+            }
+            else if (N >= 425 && N < 450)
+            {
+                Console.WriteLine("Indigo");
+            }
+            else if (N >= 380 && N < 425)
+            {
+                Console.WriteLine("Violet");
+            }
+        }
+    }
+}
