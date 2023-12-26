1bcf2bab634e6cd0c92364e809909f3b723ed00a
sange17 <sange17@naver.com>
2023-12-26 21:30:42 +0900

    Add Data_Structures_26069.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Data_Structures/Data_Structures_26069.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Data_Structures
+{
+    class Data_Structures_26069
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string userA = string.Empty;
+            string userB = string.Empty;
+
+            HashSet<string> enterUser = new HashSet<string>();
+            enterUser.Add("ChongChong");
+
+            int N = Int32.Parse(sr.ReadLine());
+
+            string line = string.Empty;
+            for(int i = 0; i < N; i++)
+            {
+                line = sr.ReadLine();
+                userA = line.Split(' ')[0];
+                userB = line.Split(' ')[1];
+
+                if(enterUser.Contains(userA) || enterUser.Contains(userB))
+                {
+                    enterUser.Add(userA);
+                    enterUser.Add(userB);
+                }
+            }
+
+            sw.WriteLine(enterUser.Count);
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Data_Structures_01253 func = new Data_Structures_01253();
+            Data_Structures_26069 func = new Data_Structures_26069();
 
             func.solve();
         }
