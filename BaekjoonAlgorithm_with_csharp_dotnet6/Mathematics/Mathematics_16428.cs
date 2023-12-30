dd1cf5b82733bcc9718c3ceb4626ba1f292648b5
sange17 <sange17@naver.com>
2023-12-30 12:01:01 +0900

    Add Mathematics_16428.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_16428.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_16428
+    {
+        public void solve()
+        {
+            string[]? input = Console.ReadLine().Split(' ');
+            BigInteger.TryParse(input[0], out BigInteger a);
+            BigInteger.TryParse(input[1], out BigInteger b);
+
+            BigInteger mod = a % b;
+            if (mod < 0)
+                mod += BigInteger.Abs(b);
+
+            Console.WriteLine((a - mod) / b);
+            Console.Write(mod);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Data_Structures_20040 func = new Data_Structures_20040();
+            Mathematics_16428 func = new Mathematics_16428();
 
             func.solve();
         }
