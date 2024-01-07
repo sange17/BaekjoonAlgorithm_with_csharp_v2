5231a35437bf89a631e9e711d93887906ae8c1bc
sange17 <sange17@naver.com>
2024-01-07 15:26:27 +0900

    Add Mathematics_24265.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_24265.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_24265
+    {
+        public void solve()
+        {
+            long N = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(N * (N - 1) / 2);
+            Console.Write(2);
+        }
+    }
+}
