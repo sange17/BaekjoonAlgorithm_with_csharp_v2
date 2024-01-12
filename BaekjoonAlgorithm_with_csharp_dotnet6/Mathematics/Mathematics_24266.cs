5b887bb7cf34dc86827bbed7f75af14d58d08573
sange17 <sange17@naver.com>
2024-01-12 21:26:01 +0900

    Add Mathematics_24266.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_24266.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_24266
+    {
+        public void solve()
+        {
+            long n = Int32.Parse(Console.ReadLine());
+
+            Console.Write(n * n * n + "\n" + 3);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_11051 func = new Mathematics_11051();
+            Mathematics_24266 func = new Mathematics_24266();
 
             func.solve();
         }
