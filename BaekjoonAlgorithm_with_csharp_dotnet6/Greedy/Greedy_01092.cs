d4e4d5acb8c1b323747ce852b533ec15cb15f76c
sange17 <sange17@naver.com>
2024-01-13 15:06:40 +0900

    Add Greedy_01092.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Greedy/Greedy_01092.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Greedy
+{
+    class Greedy_01092
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            List<int> crane = new List<int>();
+
+            string[] lineN = Console.ReadLine().Split(' ');
+            for(int i = 0; i < N; i++)
+            {
+                crane.Add(Int32.Parse(lineN[i]));
+            }
+
+            int M = Int32.Parse(Console.ReadLine());
+            List<int> box = new List<int>();
+
+            string[] lineM = Console.ReadLine().Split(' ');
+            for(int i = 0; i < M; i++)
+            {
+                box.Add(Int32.Parse(lineM[i]));
+            }
+
+            crane.Sort(new Comparison<int>((n1, n2) => n2.CompareTo(n1)));
+            box.Sort(new Comparison<int>((n1, n2) => n2.CompareTo(n1)));
+
+            if (crane[0] < box[0])
+            {
+                Console.Write(-1);
+                return;
+            }
+
+            int day = 0;
+            while(box.Count > 0)
+            {
+                int boxIdx = 0;
+                int craneIdx = 0;
+
+                while (craneIdx < N)
+                {
+                    if (boxIdx == box.Count)
+                        break;
+                    else if (crane[craneIdx] >= box[boxIdx])
+                    {
+                        box.RemoveAt(boxIdx);
+                        craneIdx++;
+                    }
+                    else
+                        boxIdx++;
+                }
+                day++;
+            }
+
+            Console.Write(day);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_24266 func = new Mathematics_24266();
+            Greedy_01092 func = new Greedy_01092();
 
             func.solve();
         }
