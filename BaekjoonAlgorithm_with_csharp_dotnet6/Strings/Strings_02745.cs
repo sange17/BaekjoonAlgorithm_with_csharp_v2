b25dd9e45bfc0779f8a8c977913ad04b07a207ab
sange17 <sange17@naver.com>
2024-02-06 20:41:41 +0900

    Add Strings_02745.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_02745.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_02745
+    {
+        public void solve()
+        {
+            string line = Console.ReadLine();
+            string N = line.Split(" ")[0];
+            int B = Int32.Parse(line.Split(" ")[1]);
+
+            int tmp = 1;
+            int sum = 0;
+
+            for(int i = N.Length - 1; i >= 0; i--)
+            {
+                if('A' <= N[i] && N[i] <= 'Z')
+                {
+                    sum += (N[i] - 'A' + 10) * tmp;
+                }
+                else
+                {
+                    sum += (N[i] - '0') * tmp;
+                }
+
+                tmp *= B;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
