cf25c916debd1946fc85871086f826f83f773eab
sange17 <sange17@naver.com>
2024-02-12 21:13:37 +0900

    Add Implementation_23804.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_23804.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_23804
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(sr.ReadLine());
+
+            for(int i = 0; i < N * 1; i++)
+            {
+                for (int j = 0; j < N * 5; j++)
+                {
+                    sw.Write("@");
+                }
+                sw.WriteLine();
+            }
+
+            for(int i = 0; i < (5 - 2) * N; i++)
+            {
+                for(int j = 0; j < N; j++)
+                {
+                    sw.Write("@");
+                }
+                sw.WriteLine();
+            }
+
+            for (int i = 0; i < N * 1; i++)
+            {
+                for (int j = 0; j < N * 5; j++)
+                {
+                    sw.Write("@");
+                }
+                sw.WriteLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_18005 func = new Mathematics_18005();
+            Implementation_23804 func = new Implementation_23804();
 
             func.solve();
         }
