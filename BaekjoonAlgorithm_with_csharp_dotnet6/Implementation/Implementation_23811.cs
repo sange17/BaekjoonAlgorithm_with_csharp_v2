05adfe4b15ec00f32f2027e8d91fcb2906a65f04
sange17 <sange17@naver.com>
2024-02-23 20:12:13 +0900

    Add Implementation_23811.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_23811.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_23811
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N * 5; j++)
+                    sw.Write("@");
+                sw.WriteLine();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    sw.Write("@");
+                sw.WriteLine();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N * 5; j++)
+                    sw.Write("@");
+                sw.WriteLine();
+            }
+
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    sw.Write("@");
+                sw.WriteLine();
+            }
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N * 5; j++)
+                    sw.Write("@");
+                sw.WriteLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_23810 func = new Implementation_23810();
+            Implementation_23811 func = new Implementation_23811();
 
             func.solve();
         }
