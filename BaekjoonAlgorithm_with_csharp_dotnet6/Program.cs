28f95ad6bc0a5a322bb1f5f70b8c61a6260e9e7e
sange17 <sange17@naver.com>
2024-02-27 20:01:58 +0900

    Add Strings_01919.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_30032 func = new Implementation_30032();
+            Strings_01919 func = new Strings_01919();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_01919.cs
@@ -0,0 +1,32 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_01919
+    {
+        public void solve()
+        {
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+            int cnt = 0;
+            int[] countStr1 = new int[26];
+            int[] countStr2 = new int[26];
+
+            for(int i = 0; i < str1.Length; i++)
+            {
+                countStr1[str1[i] - 'a']++;
+            }
+
+            for (int i = 0; i < str2.Length; i++)
+            {
+                countStr2[str2[i] - 'a']++;
+            }
+
+            for(int i = 0; i < 26; i++)
+            {
+                int compare = countStr1[i] - countStr2[i];
+                if (compare != 0) cnt += Math.Abs(compare);
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
