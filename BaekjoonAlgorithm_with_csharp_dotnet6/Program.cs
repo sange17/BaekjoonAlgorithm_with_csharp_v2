a332f08a41afdf64def7f10d4b3c90e4b758de61
sange17 <sange17@naver.com>
2024-03-06 23:07:09 +0900

    Add Strings_01032.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_02495 func = new Strings_02495();
+            Strings_01032 func = new Strings_01032();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_01032.cs
@@ -0,0 +1,35 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_01032
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+
+            string line = Console.ReadLine();
+            char[] array = line.ToCharArray();
+            if (N == 1)
+            {
+                Console.Write(line);
+            }
+            else
+            {
+                for (int i = 1; i < N; i++)
+                {
+                    line = Console.ReadLine();
+                    for (int j = 0; j < array.Length; j++)
+                    {
+                        if (array[j] != line[j])
+                        {
+                            array[j] = '?';
+                        }
+                    }
+                }
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write(array[i]);
+                }
+            }
+        }
+    }
+}
