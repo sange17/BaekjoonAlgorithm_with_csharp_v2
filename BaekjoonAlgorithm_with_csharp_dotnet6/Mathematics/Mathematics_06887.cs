c713078b604a87bfeddd4acbd350805524865be3
sange17 <sange17@naver.com>
2024-03-17 21:52:58 +0900

    Add Mathematics_06887.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_06887.cs
@@ -0,0 +1,23 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_06887
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine());
+            int answer = 0;
+
+            int i = 1;
+            while (true)
+            {
+                if (i * i > N) break;
+
+                answer = i;
+
+                i++;
+            }
+
+            Console.Write("The largest square has side length " + answer + ".");
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_05357 func = new Strings_05357();
+            Mathematics_06887 func = new Mathematics_06887();
 
             func.solve();
         }
