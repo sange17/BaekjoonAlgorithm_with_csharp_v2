d5f4f3cfcbc03e19702b154e911f225443c7fbe4
sange17 <sange17@naver.com>
2024-04-06 11:11:04 +0900

    Add Implementation_11121.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_11121.cs
@@ -0,0 +1,30 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_11121
+    {
+        static int t = 0;
+        static string a, b;
+        static string isOk()
+        {
+            for(int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i]) return "ERROR";
+            }
+            return "OK";
+        }
+
+        public void solve()
+        {
+            int t = Int32.Parse(Console.ReadLine());
+            while(t > 0)
+            {
+                string line = Console.ReadLine();
+                a = line.Split(" ")[0];
+                b = line.Split(" ")[1];
+
+                Console.WriteLine(isOk());
+                t--;
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_04459 func = new Strings_04459();
+            Implementation_11121 func = new Implementation_11121();
 
             func.solve();
         }
