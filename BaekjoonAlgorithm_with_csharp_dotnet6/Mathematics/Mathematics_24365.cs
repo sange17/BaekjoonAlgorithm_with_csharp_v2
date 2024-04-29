0bbbf4f40590f94f25d312505247ef8e4e56f651
sange17 <sange17@naver.com>
2024-04-29 21:27:37 +0900

    Add Mathematics_24365.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_24365.cs
@@ -0,0 +1,16 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_24365
+    {
+        public void solve()
+        {
+            string line = Console.ReadLine();
+            int a = Int32.Parse(line.Split(" ")[0]);
+            int b = Int32.Parse(line.Split(" ")[1]);
+            int c = Int32.Parse(line.Split(" ")[2]);
+            int k = (a + b + c) / 3;
+
+            Console.Write((k - a)+(k-(b-(k-a))));
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_31606 func = new Mathematics_31606();
+            Mathematics_24365 func = new Mathematics_24365();
 
             func.solve();
         }
