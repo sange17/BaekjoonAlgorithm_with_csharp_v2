8a427c39c76b150601c4b65c45e39e40b01733da
sange17 <sange17@naver.com>
2024-04-30 23:42:14 +0900

    Add Mathematics_31450.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_31450.cs
@@ -0,0 +1,15 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_31450
+    {
+        public void solve()
+        {
+            string line = Console.ReadLine();
+            int M = Int32.Parse(line.Split(" ")[0]);
+            int K = Int32.Parse(line.Split(" ")[1]);
+
+            if(M % K == 0) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
