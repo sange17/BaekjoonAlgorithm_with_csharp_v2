f55f6dc39a0ee82c346227c70f625cd477e68d33
sange17 <sange17@naver.com>
2024-05-11 22:15:15 +0900

    Add Strings_26768.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_27332 func = new Mathematics_27332();
+            Strings_26768 func = new Strings_26768();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_26768.cs
@@ -0,0 +1,31 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_26768
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string line = sr.ReadLine();
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 'a')
+                    sw.Write('4');
+                else if (line[i] == 'e')
+                    sw.Write('3');
+                else if (line[i] == 'i')
+                    sw.Write('1');
+                else if (line[i] == 'o')
+                    sw.Write('0');
+                else if (line[i] == 's')
+                    sw.Write('5');
+                else
+                    sw.Write(line[i]);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
