225d973108f737fa45b2200af8df1ba333f65d07
sange17 <sange17@naver.com>
2024-05-25 19:21:00 +0900

    Add Mathematics_26561.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_26561.cs
@@ -0,0 +1,23 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_26561
+    {
+        public void solve()
+        {
+            int T = Int32.Parse(Console.ReadLine());
+
+            for(int i = 0; i < T; i++)
+            {
+                string[] arr = Console.ReadLine().Split(" ");
+                int population = Int32.Parse(arr[0]);
+                int time = Int32.Parse(arr[1]);
+
+                int birth = time / 4;
+                int death = time / 7;
+
+                population += (birth - death);
+                Console.WriteLine(population);
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_27590 func = new Implementation_27590();
+            Mathematics_26561 func = new Mathematics_26561();
 
             func.solve();
         }
