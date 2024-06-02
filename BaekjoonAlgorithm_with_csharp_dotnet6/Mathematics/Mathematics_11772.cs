d3c5f3f3242b98e3ea7f7e2aac398c475d2a3771
sange17 <sange17@naver.com>
2024-06-02 13:12:53 +0900

    Add Mathematics_11772.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_11772.cs
@@ -0,0 +1,25 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_11772
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                double num = double.Parse(line.Substring(0, line.Length - 1));
+                double pot = double.Parse(line.Substring(line.Length - 1));
+
+                if (pot == 0)
+                    sum += 1;
+                else
+                    sum += Math.Pow(num, pot);
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_30067 func = new Mathematics_30067();
+            Mathematics_11772 func = new Mathematics_11772();
 
             func.solve();
         }
