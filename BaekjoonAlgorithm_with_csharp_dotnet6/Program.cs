fd77c0d494d1dbd614861ed6913b05879a9f2972
sange17 <sange17@naver.com>
2024-06-13 22:49:47 +0900

    Add Strings_08371.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_24267 func = new Mathematics_24267();
+            Strings_08371 func = new Strings_08371();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_08371.cs
@@ -0,0 +1,21 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_08371
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (str1[i] != str2[i])
+                    count++;
+            }
+
+            Console.Write(count);
+        }
+    }
+}
