0a684d9dc2d2bfc1b72ee74579db921973c8b793
sange17 <sange17@naver.com>
2024-07-04 23:13:35 +0900

    Add Strings_26004.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_24569 func = new Mathematics_24569();
+            Strings_26004 func = new Strings_26004();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_26004.cs
@@ -0,0 +1,35 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_26004
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine());
+            string line = Console.ReadLine();
+            int H = 0;
+            int I = 0;
+            int A = 0;
+            int R = 0;
+            int C = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                switch (line[i])
+                {
+                    case 'H':
+                        H++; break;
+                    case 'I':
+                        I++; break;
+                    case 'A':
+                        A++; break;
+                    case 'R':
+                        R++; break;
+                    case 'C':
+                        C++; break;
+                }
+            }
+
+            Console.Write(Math.Min(H, Math.Min(I, Math.Min(A, Math.Min(R, C)))));
+        }
+    }
+}
