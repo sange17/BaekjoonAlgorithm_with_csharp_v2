f53966b7e8822e10c857c0939ce2b5c17228cc83
sange17 <sange17@naver.com>
2024-07-08 22:05:16 +0900

    Add Strings_26264.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_23348 func = new Mathematics_23348();
+            Strings_26264 func = new Strings_26264();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_26264.cs
@@ -0,0 +1,32 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_26264
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+            string line = Console.ReadLine()!;
+
+            int bigdata = 0;
+            int security = 0;
+            for(int i = 0; i < line.Length; i++)
+            {
+                if (line[i] == 's')
+                {
+                    security++;
+                }
+                else if (line[i] == 'b')
+                {
+                    bigdata++;
+                }
+            }
+
+            if (bigdata == security)
+                Console.Write("bigdata? security!");
+            else if (bigdata < security)
+                Console.Write("security!");
+            else if (bigdata > security)
+                Console.Write("bigdata?");
+        }
+    }
+}
