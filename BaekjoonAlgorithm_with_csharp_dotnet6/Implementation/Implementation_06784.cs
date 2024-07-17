aadc25744c2cc60f7e03a818aae910ab31897106
sange17 <sange17@naver.com>
2024-07-17 20:32:22 +0900

    Add Implementation_06784.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_06784.cs
@@ -0,0 +1,27 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_06784
+    {
+        public void solve()
+        {
+            int n = int.Parse(Console.ReadLine()!);
+
+            string[] lst = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                lst[i] = Console.ReadLine()!;
+            }
+
+            int ans = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (lst[i] == Console.ReadLine()!)
+                {
+                    ans++;
+                }
+            }
+
+            Console.WriteLine(ans);
+        }
+    }
+}
