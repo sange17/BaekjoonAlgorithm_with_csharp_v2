1d227c81f5d666b5c448f1b5bf198cde6e9fd953
sange17 <sange17@naver.com>
2024-07-19 23:01:16 +0900

    Add Implementation_18856.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_18856.cs
@@ -0,0 +1,26 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_18856
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = Int32.Parse(sr.ReadLine()!);
+
+            sw.WriteLine(n);
+            sw.Write("1 2 ");
+
+            for(int i = 0; i < n - 3; i++)
+            {
+                sw.Write((3 + i) + " ");
+            }
+
+            sw.Write(53);
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
