6814363f85526e8cbee3a286ca32ee047f182137
sange17 <sange17@naver.com>
2024-07-21 22:08:04 +0900

    Add Mathematics_23530.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_23530.cs
@@ -0,0 +1,19 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_23530
+    {
+        public void solve()
+        {
+            int t = Int32.Parse(Console.ReadLine()!);
+
+            for (int i = 0; i < t; i++)
+            {
+                string line = Console.ReadLine()!;
+                int a = Int32.Parse(line.Split(" ")[0]);
+                int b = Int32.Parse(line.Split(" ")[1]);
+
+                Console.WriteLine((a + b) / 2);
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_19751 func = new Mathematics_19751();
+            Mathematics_23530 func = new Mathematics_23530();
 
             func.solve();
         }
