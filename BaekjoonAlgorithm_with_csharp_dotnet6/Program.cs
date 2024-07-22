df35e570ba4bc1c4fce8793cd9de50c17fdc0336
sange17 <sange17@naver.com>
2024-07-22 23:02:06 +0900

    Add Strings_21955.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_23530 func = new Mathematics_23530();
+            Strings_21955 func = new Strings_21955();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_21955.cs
@@ -0,0 +1,13 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_21955
+    {
+        public void solve()
+        {
+            string n = Console.ReadLine()!;
+            int length = n.Length;
+
+            Console.Write(n.Substring(0, length / 2) + " " + n.Substring(length / 2, length / 2));
+        }
+    }
+}
