fa63b88942184278ab948312f7e0262d5c1d4bee
sange17 <sange17@naver.com>
2024-07-23 23:48:07 +0900

    Add Strings_23080.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_21955 func = new Strings_21955();
+            Strings_23080 func = new Strings_23080();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_23080.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_23080
+    {
+        public void solve()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            int n = int.Parse(Console.ReadLine());
+
+            string line = Console.ReadLine();
+
+            for (int i = 0; i < line.Length; i += n)
+            {
+                sb.Append(line[i]);
+            }
+
+            Console.Write(sb.ToString());
+        }
+    }
+}
