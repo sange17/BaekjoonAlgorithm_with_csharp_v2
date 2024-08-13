c6824e8e9badad766bc044d541f99b6e85d5e88f
sange17 <sange17@naver.com>
2024-08-13 21:54:48 +0900

    Add Strings_14209.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_08721 func = new Implementation_08721();
+            Strings_14209 func = new Strings_14209();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_14209.cs
@@ -0,0 +1,27 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_14209
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine()!);
+
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine()!;
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (line[j] == 'A') sum += 4;
+                    else if (line[j] == 'K') sum += 3;
+                    else if (line[j] == 'Q') sum += 2;
+                    else if (line[j] == 'J') sum += 1;
+                    else if (line[j] == 'X') sum += 0;
+                }
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
