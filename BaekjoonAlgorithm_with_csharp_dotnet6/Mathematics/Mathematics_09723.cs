33e4aef0aaf44a3a3128495f71aa9cd421542945
sange17 <sange17@naver.com>
2024-08-14 21:22:24 +0900

    Add Mathematics_09723.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_09723.cs
@@ -0,0 +1,27 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_09723
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(sr.ReadLine()!);
+
+            for (int i = 1; i <= N; i++)
+            {
+                int[] array = sr.ReadLine()!.Split().Select(int.Parse).ToArray();
+                Array.Sort(array);
+
+                if (array[2] * array[2] == array[0] * array[0] + array[1] * array[1])
+                    sw.WriteLine("Case #" + i + ": YES");
+                else
+                    sw.WriteLine("Case #" + i + ": NO");
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_14209 func = new Strings_14209();
+            Mathematics_09723 func = new Mathematics_09723();
 
             func.solve();
         }
