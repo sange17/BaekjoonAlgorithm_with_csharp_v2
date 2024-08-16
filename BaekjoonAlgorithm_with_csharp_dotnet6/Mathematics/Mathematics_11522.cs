b26e525ed1229c3fc3204c270b940c2320e20605
sange17 <sange17@naver.com>
2024-08-16 23:21:18 +0900

    Add Mathematics_11522.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_11522.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_11522
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(sr.ReadLine()!);
+
+            for (int i = 0; i < N; i++)
+            {
+                int[] inputs = sr.ReadLine()!.Split().Select(int.Parse).ToArray();
+                int sum = 0;
+
+                for (int j = 1; j <= inputs[1]; j++)
+                {
+                    sum += j;
+                }
+
+                sw.WriteLine(inputs[0] + " " + sum + " " + (inputs[1] * inputs[1]) + " " + (inputs[1] * inputs[1] + inputs[1]));
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_13073 func = new Mathematics_13073();
+            Mathematics_11522 func = new Mathematics_11522();
 
             func.solve();
         }
