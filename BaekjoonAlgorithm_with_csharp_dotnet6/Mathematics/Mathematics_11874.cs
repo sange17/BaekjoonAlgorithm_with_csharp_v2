ba0417ca23418b0c53fcd8f2e545771273617db6
sange17 <sange17@naver.com>
2024-08-19 10:53:57 +0900

    Add Mathematics_11874.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_11874.cs
@@ -0,0 +1,43 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_11874
+    {
+        public void solve()
+        {
+            var l = int.Parse(Console.ReadLine()!);
+            var d = int.Parse(Console.ReadLine()!);
+            var x = int.Parse(Console.ReadLine()!);
+
+            Func<int, int> SumDigits = (num) => {
+                int ret = 0;
+                while (true)
+                {
+                    if (num == 0) break;
+
+                    ret += num % 10;
+                    num /= 10;
+                }
+
+                return ret;
+            };
+
+            Func<int, bool> isSameToX = (i) => SumDigits(i) == x;
+
+            for (int i = l; i <= d; i++)
+            {
+                if (isSameToX(i))
+                {
+                    Console.WriteLine(i); break;
+                }
+            }
+
+            for (int i = d; i >= l; i--)
+            {
+                if (isSameToX(i))
+                {
+                    Console.WriteLine(i); break;
+                }
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_05751 func = new Implementation_05751();
+            Mathematics_11874 func = new Mathematics_11874();
 
             func.solve();
         }
