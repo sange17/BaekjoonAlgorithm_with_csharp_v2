eb3376f10bc05a2d9e77203e52cd5f450d0e5912
sange17 <sange17@naver.com>
2024-08-22 23:07:47 +0900

    Add Mathematics_14682.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_14682.cs
@@ -0,0 +1,20 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_14682
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+            int k = Int32.Parse(Console.ReadLine()!);
+
+            int sum = n;
+            for (int i = 0; i < k; i++)
+            {
+                sum += Int32.Parse(n + "0");
+                n = Int32.Parse(n + "0");
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_13484 func = new Implementation_13484();
+            Mathematics_14682 func = new Mathematics_14682();
 
             func.solve();
         }
