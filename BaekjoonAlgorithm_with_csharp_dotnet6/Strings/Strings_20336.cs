f1ba51486cd65c2bcd14eaf643f038f29d087c01
sange17 <sange17@naver.com>
2024-08-26 21:46:07 +0900

    Add Strings_20336.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_20336.cs
@@ -0,0 +1,22 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_20336
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+
+            string[] array = Console.ReadLine()!.Split(' ');
+
+            for (int i = 1; i < n; i++)
+            {
+                array = Console.ReadLine()!.Split(' ');
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
