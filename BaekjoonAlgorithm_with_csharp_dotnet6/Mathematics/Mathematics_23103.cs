83b317f9fffce7ec9db23c3a63263c330688bc38
sange17 <sange17@naver.com>
2024-09-01 21:00:20 +0900

    Add Mathematics_23103,cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_23103.cs
@@ -0,0 +1,30 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_23103
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+
+            string line = Console.ReadLine()!;
+            int xi1 = Int32.Parse(line.Split(" ")[0]);
+            int yi1 = Int32.Parse(line.Split(" ")[1]);
+
+            int sum = 0;
+            for (int i = 0; i < n - 1; i++)
+            {
+                line = Console.ReadLine()!;
+
+                int xi2 = Int32.Parse(line.Split(" ")[0]);
+                int yi2 = Int32.Parse(line.Split(" ")[1]);
+
+                sum += (Math.Abs(xi1 - xi2) + Math.Abs(yi1 - yi2));
+
+                xi1 = xi2;
+                yi1 = yi2;
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_24333 func = new Implementation_24333();
+            Mathematics_23103 func = new Mathematics_23103();
 
             func.solve();
         }
