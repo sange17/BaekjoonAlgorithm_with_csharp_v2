3a9c6f5f0ec2322fe63057663c9fa56e30ffe64c
sange17 <sange17@naver.com>
2024-09-07 21:36:32 +0900

    Add Strings_25801.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_24923 func = new Strings_24923();
+            Strings_25801 func = new Strings_25801();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_25801.cs
@@ -0,0 +1,40 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_25801
+    {
+        public void solve()
+        {
+            char[] chars = Console.ReadLine()!.ToCharArray();
+
+            int[] array = new int[26];
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                array[chars[i] - 97]++;
+            }
+
+            int cntEven = 0;
+            int cntOdd = 0;
+            for (int i = 0; i < 26; i++)
+            {
+                if (array[i] == 0) continue;
+
+                if (array[i] % 2 == 0) cntEven++;
+                else if(array[i] % 2 != 0) cntOdd++;
+            }
+
+            if(cntEven > 0 &&  cntOdd == 0)
+            {
+                Console.Write("0");
+            }
+            else if (cntEven == 0 && cntOdd > 0)
+            {
+                Console.Write("1");
+            }
+            else if (cntEven > 0 && cntOdd > 0)
+            {
+                Console.Write("0/1");
+            }
+        }
+    }
+}
