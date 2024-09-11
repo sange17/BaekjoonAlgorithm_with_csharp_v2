77630457f8ff3a52bb242873a001538780719af6
sange17 <sange17@naver.com>
2024-09-11 22:29:55 +0900

    Add Mathematics_14579.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_14579.cs
@@ -0,0 +1,21 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_14579
+    {
+        public void solve()
+        {
+            int[] inputs = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+
+            int sum = 0;
+            int result = 1;
+            for (int i = inputs[0]; i <= inputs[1]; i++)
+            {
+                sum = i * (i + 1) / 2;
+                result *= sum;
+                result %= 14579;
+            }
+
+            Console.Write(result);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_25870 func = new Strings_25870();
+            Mathematics_14579 func = new Mathematics_14579();
 
             func.solve();
         }
