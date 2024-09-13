38e410900a30fbc1af927bacd5e538c95f60483e
sange17 <sange17@naver.com>
2024-09-13 22:53:00 +0900

    Add Implementation_16316.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_16316.cs
@@ -0,0 +1,28 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_16316
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+            string[] array = Console.ReadLine()!.Split(" ");
+
+            bool flag = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] == "mumble" || Int32.Parse(array[i]) == (i + 1)) 
+                    continue;
+                else if (Int32.Parse(array[i]) != (i + 1))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag)
+                Console.Write("makes sense");
+            else
+                Console.Write("something is fishy");
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_24603 func = new Mathematics_24603();
+            Implementation_16316 func = new Implementation_16316();
 
             func.solve();
         }
