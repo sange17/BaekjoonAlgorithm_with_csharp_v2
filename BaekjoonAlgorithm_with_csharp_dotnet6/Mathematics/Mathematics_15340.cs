6b0142313e86434fab517d3ec01b6ceb86bd001e
sange17 <sange17@naver.com>
2024-09-22 19:14:45 +0900

    Add Mathematics_15340.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_15340.cs
@@ -0,0 +1,32 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_15340
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            while (true)
+            {
+                int[] array = sr.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+                if (array[0] == 0 && array[1] == 0)
+                {
+                    sr.Close();
+                    sw.Close();
+                    break;
+                }
+
+                int[] prices = new int[3];
+                prices[0] = array[0] * 30 + array[1] * 40;
+                prices[1] = array[0] * 35 + array[1] * 30;
+                prices[2] = array[0] * 40 + array[1] * 20;
+
+                Array.Sort(prices);
+
+                sw.WriteLine(prices[0]);
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -14,7 +14,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_04635 func = new Mathematics_04635();
+            Mathematics_15340 func = new Mathematics_15340();
 
             func.solve();
         }
