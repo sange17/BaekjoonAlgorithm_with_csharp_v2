2f3ce85f831d166adefeaa015daebc433a172ebf
sange17 <sange17@naver.com>
2024-10-04 22:32:55 +0900

    Add Strings_29731.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_08387 func = new Strings_08387();
+            Strings_29731 func = new Strings_29731();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_29731.cs
@@ -0,0 +1,34 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_29731
+    {
+        public void solve()
+        {
+            string[] array = { "Never gonna give you up"
+                              ,"Never gonna let you down"
+                              ,"Never gonna run around and desert you"
+                              ,"Never gonna make you cry"
+                              ,"Never gonna say goodbye"
+                              ,"Never gonna tell a lie and hurt you"
+                              ,"Never gonna stop"
+                             };
+
+            int n = Int32.Parse(Console.ReadLine()!);
+
+            bool check = true;
+            for(int i = 0; i < n; i++)
+            {
+                if (!array.Contains(Console.ReadLine()!))
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (!check)
+                Console.Write("Yes");
+            else
+                Console.Write("No");
+        }
+    }
+}
