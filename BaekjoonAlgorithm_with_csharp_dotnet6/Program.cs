b5d937c2058892ac1d81bed8e185203702493806
sange17 <sange17@naver.com>
2024-10-05 12:26:52 +0900

    Add Strings_07366.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_29731 func = new Strings_29731();
+            Strings_07366 func = new Strings_07366();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_07366.cs
@@ -0,0 +1,29 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_07366
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int t = Int32.Parse(sr.ReadLine()!);
+            for (int i = 1; i <= t; i++)
+            {
+                int n = Int32.Parse(sr.ReadLine()!);
+                int count = 0;
+                string[] array = sr.ReadLine()!.Split(" ").ToArray();
+                for (int j = 0; j < n; j++)
+                {
+                    if (array[j] == "sheep") count++;
+                }
+
+                sw.WriteLine("Case " + i + ": This list contains " + count + " sheep.");
+                sw.WriteLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
