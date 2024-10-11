d924ca9c7fd569b4819c3a6f93f960899cd4eedb
sange17 <sange17@naver.com>
2024-10-11 22:57:05 +0900

    Add Implementation_02948.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_02948.cs
@@ -0,0 +1,14 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_02948
+    {
+        public void solve()
+        {
+            string[] date = Console.ReadLine()!.Split(" ");
+
+            DateTime dateTime = DateTime.Parse("2009-" + date[1].PadLeft(2,'0') + "-" + date[0].PadLeft(2,'0'));
+
+            Console.Write(dateTime.DayOfWeek);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_02774 func = new Implementation_02774();
+            Implementation_02948 func = new Implementation_02948();
 
             func.solve();
         }
