a87ed79933bf61e2726f37f0fae68ed444fe06ad
sange17 <sange17@naver.com>
2024-10-13 20:24:11 +0900

    Add Implementation_08712.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_08712.cs
@@ -0,0 +1,40 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_08712
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(sr.ReadLine()!);
+
+            int num = 1;
+            for (int i = 1; i <= N; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    int temp = num;
+                    for (int j = N - 1; j >= 0; j--)
+                    {
+                        sw.Write((temp + j) + " ");
+                        num++;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        sw.Write(num + " ");
+                        num++;
+                    }
+                }
+
+                sw.WriteLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_05363 func = new Strings_05363();
+            Implementation_08712 func = new Implementation_08712();
 
             func.solve();
         }
