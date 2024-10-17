d68b44eaf75b99126f084880c518f05806f665ee
sange17 <sange17@naver.com>
2024-10-17 21:30:11 +0900

    Add Implementation_01173.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_01173.cs
@@ -0,0 +1,47 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_01173
+    {
+        public void solve()
+        {
+            int[] array = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int time = 0;
+            int pulse = array[1];
+            if (array[1] + array[3] > array[2])
+            {
+                Console.Write(-1);
+            }
+            else
+            {
+                while (true)
+                {
+                    if (array[0] == 0)
+                    {
+                        Console.Write(time);
+                        break;
+                    }
+
+                    if (pulse + array[3] <= array[2])
+                    {
+                        time++;
+                        array[0]--;
+                        pulse += array[3];
+                    }
+                    else
+                    {
+                        if (pulse - array[4] < array[1])
+                        {
+                            pulse = array[1];
+                        }
+                        else
+                        {
+                            pulse -= array[4];
+                        }
+                        time++;
+                    }
+                }
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_05919 func = new Mathematics_05919();
+            Implementation_01173 func = new Implementation_01173();
 
             func.solve();
         }
