8dff20c2ca55eeace09e9e8d802754b377213f3e
sange17 <sange17@naver.com>
2024-10-23 07:54:05 +0900

    Add Implementation_15051.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_15051.cs
@@ -0,0 +1,18 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_15051
+    {
+        public void solve()
+        {
+            int a1 = Int32.Parse(Console.ReadLine()!);
+            int a2 = Int32.Parse(Console.ReadLine()!);
+            int a3 = Int32.Parse(Console.ReadLine()!);
+
+            int case1 = a2 * 2 + a3 * 4;
+            int case2 = a1 * 2 + a3 * 2;
+            int case3 = a1 * 4 + a2 * 2;
+
+            Console.Write(Math.Min(Math.Min(case1, case2), case3));
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -15,7 +15,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_01681 func = new Implementation_01681();
+            Implementation_15051 func = new Implementation_15051();
 
             func.solve();
         }
