1b3a134557cb79252266501985e2452f4fd56abd
sange17 <sange17@naver.com>
2024-11-27 20:09:34 +0900

    Add Strings_01871.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_07600 func = new Strings_07600();
+            Strings_01871 func = new Strings_01871();
 
             func.solve();
         }
