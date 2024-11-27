264bbebe907801a1379cab2e0ec3a7162f827b7c
sange17 <sange17@naver.com>
2024-11-28 07:31:16 +0900

    Add Strings_02703.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_01871 func = new Strings_01871();
+            Strings_02703 func = new Strings_02703();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_02703.cs
@@ -0,0 +1,30 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_02703
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int T = Int32.Parse(sr.ReadLine()!);
+            for (int i = 0; i < T; i++)
+            {
+                string msgLine = sr.ReadLine()!;
+                char[] ruleArr = sr.ReadLine()!.ToCharArray();
+
+                for (int j = 0; j < msgLine.Length; j++)
+                {
+                    if(msgLine[j] == ' ')
+                        sw.Write(' ');
+                    else
+                        sw.Write(ruleArr[msgLine[j] - 65]);
+                }
+                sw.WriteLine();
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
