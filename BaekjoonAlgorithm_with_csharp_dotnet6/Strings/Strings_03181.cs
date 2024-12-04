7640d525b92abe977c9b5a15459a23350bd157b4
sange17 <sange17@naver.com>
2024-12-05 06:01:31 +0900

    Add Strings_03181.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_03181.cs
@@ -0,0 +1,25 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_03181
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            string[] array = new string[] { "i", "pa", "te", "ni", "niti", "a", "ali", "nego", "no", "ili" };
+            string[] inputArr = sr.ReadLine()!.Split(" ");
+
+            for (int i = 0; i < inputArr.Length; i++)
+            {
+                if (array.Contains(inputArr[i]) && i != 0) continue;
+
+                string temp = inputArr[i].ToUpper();
+                sw.Write(temp[0]);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
