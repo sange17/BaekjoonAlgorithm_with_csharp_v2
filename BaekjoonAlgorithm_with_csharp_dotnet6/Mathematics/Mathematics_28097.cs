5409a536fd3168bf4993983a24dda9cc67865d72
sange17 <sange17@naver.com>
2024-12-16 07:32:14 +0900

    Add Mathematics_28097.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_28097.cs
@@ -0,0 +1,13 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_28097
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+            int[] array = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+            int sum = array.Sum() + (array.Length - 1) * 8;
+            Console.Write(sum / 24 + " " + sum % 24);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_25591 func = new Mathematics_25591();
+            Mathematics_28097 func = new Mathematics_28097();
 
             func.solve();
         }
