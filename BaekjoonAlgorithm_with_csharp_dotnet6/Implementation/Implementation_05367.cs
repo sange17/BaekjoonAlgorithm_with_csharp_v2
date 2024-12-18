a180949736c069f67763abb0efa19494d6d84b17
sange17 <sange17@naver.com>
2024-12-19 07:47:08 +0900

    Add Implementation_05367.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_05367.cs
@@ -0,0 +1,42 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_05367
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = Int32.Parse(Console.ReadLine()!);
+
+            sw.Write("|");
+            for (int i = 0; i < n - 2; i++)
+                sw.Write("-");
+            sw.WriteLine("|");
+
+            int left = 1;
+            int right = n - 2;
+            for (int i = 0; i < n - 2; i++)
+            {
+                sw.Write("|");
+                for (int j = 1; j < n - 1; j++)
+                {
+                    if(j == left || j == right) sw.Write("*");
+                    else sw.Write(" ");
+                }
+                sw.WriteLine("|");
+
+                left++;
+                right--;
+            }
+
+            sw.Write("|");
+            for (int i = 0; i < n - 2; i++)
+                sw.Write("-");
+            sw.Write("|");
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_30979 func = new Mathematics_30979();
+            Implementation_05367 func = new Implementation_05367();
 
             func.solve();
         }
