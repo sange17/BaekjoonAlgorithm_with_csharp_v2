8501cc3167bd68be9d9250fb1538a8f38c424bab
sange17 <sange17@naver.com>
2024-12-22 12:18:35 +0900

    Add Mathematics_29807.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_29807.cs
@@ -0,0 +1,61 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_29807
+    {
+        public void solve()
+        {
+            int T = Int32.Parse(Console.ReadLine()!);
+            int[] array = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+            int result = 0;
+            if (T == 1)
+            {
+                sum1 = array[0] * 508;
+            }
+            else if (T == 2)
+            {
+                sum1 = array[0] * 508;
+                sum2 = array[1] * 212;
+            }
+            else if(T == 3)
+            {
+                if (array[0] > array[2])
+                    sum1 = (array[0] - array[2]) * 508;
+                else
+                    sum1 = (array[2] - array[0]) * 108;
+                sum2 = array[1] * 212;
+            }
+            else if (T == 4)
+            {
+                if (array[0] > array[2])
+                    sum1 = (array[0] - array[2]) * 508;
+                else
+                    sum1 = (array[2] - array[0]) * 108;
+                if (array[1] > array[3])
+                    sum2 = (array[1] - array[3]) * 212;
+                else
+                    sum2 = (array[3] - array[1]) * 305;
+            }
+
+            else if (T == 5)
+            {
+                if (array[0] > array[2])
+                    sum1 = (array[0] - array[2]) * 508;
+                else
+                    sum1 = (array[2] - array[0]) * 108;
+                if (array[1] > array[3])
+                    sum2 = (array[1] - array[3]) * 212;
+                else
+                    sum2 = (array[3] - array[1]) * 305;
+
+                sum3 = array[4] * 707;
+            }
+
+            result = (sum1 + sum2 + sum3) * 4763;
+            Console.Write(result);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_29736 func = new Mathematics_29736();
+            Mathematics_29807 func = new Mathematics_29807();
 
             func.solve();
         }
