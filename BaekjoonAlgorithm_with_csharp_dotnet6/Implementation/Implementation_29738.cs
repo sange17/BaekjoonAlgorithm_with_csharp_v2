523878433ffba655c986cf38647d6e768369f724
sange17 <sange17@naver.com>
2024-12-24 19:55:56 +0900

    Add Implementation_29738.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_29738.cs
@@ -0,0 +1,28 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_29738
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int T = Int32.Parse(sr.ReadLine()!);
+            for (int i = 1; i <= T; i++)
+            {
+                int N = Int32.Parse(sr.ReadLine()!);
+                if (N > 4500)
+                    sw.WriteLine("Case #" + i + ": Round 1");
+                else if (N > 1000 && N <= 4500)
+                    sw.WriteLine("Case #" + i + ": Round 2");
+                else if (N > 25 && N <= 1000)
+                    sw.WriteLine("Case #" + i + ": Round 3");
+                else
+                    sw.WriteLine("Case #" + i + ": World Finals");
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_06246 func = new Implementation_06246();
+            Implementation_29738 func = new Implementation_29738();
 
             func.solve();
         }
