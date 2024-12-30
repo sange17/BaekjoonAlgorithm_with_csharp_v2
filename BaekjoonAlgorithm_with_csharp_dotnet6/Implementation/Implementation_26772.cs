ed32a485b6186d6339649fefc9ce7cba8baf9f12
sange17 <sange17@naver.com>
2024-12-31 07:56:06 +0900

    Add Implementation_26772.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_26772.cs
@@ -0,0 +1,34 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_26772
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = Int32.Parse(sr.ReadLine()!);
+            for (int i = 0; i < n; i++) sw.Write(" @@@   @@@  ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("@   @ @   @ ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("@    @    @ ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("@         @ ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write(" @       @  ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("  @     @   ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("   @   @    ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("    @ @     ");
+            sw.WriteLine();
+            for (int i = 0; i < n; i++) sw.Write("     @      ");
+            sw.WriteLine();
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
