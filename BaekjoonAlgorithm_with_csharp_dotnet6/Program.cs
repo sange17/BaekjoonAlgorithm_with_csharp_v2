040df64997232eb70cb9d594b1878966afa10367
sange17 <sange17@naver.com>
2025-01-03 07:47:02 +0900

    Add Strings_24084.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_28808 func = new Strings_28808();
+            Strings_24084 func = new Strings_24084();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_24084.cs
@@ -0,0 +1,20 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_24084
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int N = Int32.Parse(sr.ReadLine()!);
+            string S = sr.ReadLine()!;
+
+            for (int i = 1; i < N; i++)
+                if (S[i] == 'J') sw.WriteLine(S[i - 1]);
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
