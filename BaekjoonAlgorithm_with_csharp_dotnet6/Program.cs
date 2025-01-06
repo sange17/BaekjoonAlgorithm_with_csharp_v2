0f2130a5ba50a0da8e80ade648e17391b3623684
sange17 <sange17@naver.com>
2025-01-06 21:32:17 +0900

    Add Strings_05013.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_10406 func = new Implementation_10406();
+            Strings_05013 func = new Strings_05013();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_05013.cs
@@ -0,0 +1,19 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_05013
+    {
+        public void solve()
+        {
+            int N = Int32.Parse(Console.ReadLine()!);
+            int count = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine()!;
+                if (!line.Contains("CD")) count++;
+            }
+
+            Console.Write(count);
+        }
+    }
+}
