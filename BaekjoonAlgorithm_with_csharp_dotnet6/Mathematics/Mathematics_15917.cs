bae75e250f843507277120bc0deeac286ddd603f
sange17 <sange17@naver.com>
2025-01-11 21:50:41 +0900

    Add Mathematics_15917.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_15917.cs
@@ -0,0 +1,29 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_15917
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int Q = Int32.Parse(sr.ReadLine()!);
+            int answer = 0;
+            
+            for (int i = 0; i < Q; i++)
+            {
+                long num = long.Parse(sr.ReadLine()!);
+
+                if ((num & (num - 1)) == 0)
+                    answer = 1;
+                else
+                    answer = 0;
+
+                sw.WriteLine(answer);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_18412 func = new Strings_18412();
+            Mathematics_15917 func = new Mathematics_15917();
 
             func.solve();
         }
