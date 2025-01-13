35d2cee9faa38390ecaf57b32b14bf6a3f572e08
sange17 <sange17@naver.com>
2025-01-13 20:07:01 +0900

    Add Mathematics_15080.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_15080.cs
@@ -0,0 +1,19 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_15080
+    {
+        public void solve()
+        {
+            int[] time1 = Console.ReadLine()!.Split(" : ").Select(int.Parse).ToArray();
+            int[] time2 = Console.ReadLine()!.Split(" : ").Select(int.Parse).ToArray();
+
+            int s1 = time1[0] * 3600 + time1[1] * 60 + time1[2];
+            int s2= time2[0] * 3600 + time2[1] * 60 + time2[2];
+
+            if(s1 > s2)
+                Console.Write(s2 - s1 + 3600 * 24);
+            else
+                Console.Write(s2 - s1);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_13163 func = new Strings_13163();
+            Mathematics_15080 func = new Mathematics_15080();
 
             func.solve();
         }
