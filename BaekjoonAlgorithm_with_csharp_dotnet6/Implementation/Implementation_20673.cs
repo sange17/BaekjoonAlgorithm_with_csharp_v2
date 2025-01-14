7e82f8961a7166e61b52b933c79c8ebc6ca3ecc7
sange17 <sange17@naver.com>
2025-01-14 20:49:08 +0900

    Add Implementation_20673.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_20673.cs
@@ -0,0 +1,18 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_20673
+    {
+        public void solve()
+        {
+            int p = Int32.Parse(Console.ReadLine()!);
+            int q = Int32.Parse(Console.ReadLine()!);
+
+            if(p <= 50 && q <= 10)
+                Console.Write("White");
+            else if(q > 30)
+                Console.Write("Red");
+            else
+                Console.Write("Yellow");
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_15080 func = new Mathematics_15080();
+            Implementation_20673 func = new Implementation_20673();
 
             func.solve();
         }
