efd92cc2a2470cc937d78bae7c9f1d09c81a8e85
sange17 <sange17@naver.com>
2025-01-18 12:59:50 +0900

    Add Strings_18698.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_28290 func = new Strings_28290();
+            Strings_18698 func = new Strings_18698();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_18698.cs
@@ -0,0 +1,23 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_18698
+    {
+        public void solve()
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+
+            int n = Int32.Parse(sr.ReadLine()!);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] array = sr.ReadLine()!.Split("D");
+
+                sw.WriteLine(array[0].Length);
+            }
+
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
