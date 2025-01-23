e9382586b5f9be60da255d9719fb31c0f45096ac
sange17 <sange17@naver.com>
2025-01-23 22:48:31 +0900

    Add Mathematics_33170.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_33170.cs
@@ -0,0 +1,17 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_33170
+    {
+        public void solve()
+        {
+            int a = Int32.Parse(Console.ReadLine()!);
+            int b = Int32.Parse(Console.ReadLine()!);
+            int c = Int32.Parse(Console.ReadLine()!);
+
+            if (a + b + c <= 21)
+                Console.WriteLine(1);
+            else
+                Console.WriteLine(0);
+        }
+    }
+}
