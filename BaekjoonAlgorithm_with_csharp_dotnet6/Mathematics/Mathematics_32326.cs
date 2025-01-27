b82605e1ee8addf081b6ed4b44e0657e1defb6cd
sange17 <sange17@naver.com>
2025-01-27 22:52:39 +0900

    Add Mathematics_32326.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_32326.cs
@@ -0,0 +1,14 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_32326
+    {
+        public void solve()
+        {
+            int R = Int32.Parse(Console.ReadLine());
+            int G = Int32.Parse(Console.ReadLine());
+            int B = Int32.Parse(Console.ReadLine());
+
+            Console.Write(R * 3 + G * 4 + B * 5);
+        }
+    }
+}
