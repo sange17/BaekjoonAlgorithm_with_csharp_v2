40038dc0d6db4cc53eb27a2ec5b67afb627c3d3d
sange17 <sange17@naver.com>
2025-02-14 21:08:20 +0900

    Add Implementation_33162.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_33162.cs
@@ -0,0 +1,19 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_33162
+    {
+        public void solve()
+        {
+            int X = Int32.Parse(Console.ReadLine()!);
+            int M = 0;
+
+            for (int i = 1; i <= X; i++)
+            {
+                if (i % 2 != 0) M += 3;
+                else if (i % 2 == 0) M -= 2;
+            }
+
+            Console.Write(M);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_33166 func = new Mathematics_33166();
+            Implementation_33162 func = new Implementation_33162();
 
             func.solve();
         }
