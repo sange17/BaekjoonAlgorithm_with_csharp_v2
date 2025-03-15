a88f2c68869b363c9769dbea64b791d8b4bf615f
sange17 <sange17@naver.com>
2025-03-15 18:27:59 +0900

    Add Implementation_31994.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_31994.cs
@@ -0,0 +1,25 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_31994
+    {
+        public void solve()
+        {
+            int max = 0;
+            string result = "";
+            for (int i = 0; i < 7; i++)
+            {
+                string line = Console.ReadLine()!;
+                string seminar = line.Split(" ")[0];
+                int people = Int32.Parse(line.Split(" ")[1]);
+
+                if (max < people)
+                {
+                    max = people;
+                    result = seminar;
+                }
+            }
+
+            Console.Write(result);
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_32288 func = new Strings_32288();
+            Implementation_31994 func = new Implementation_31994();
 
             func.solve();
         }
