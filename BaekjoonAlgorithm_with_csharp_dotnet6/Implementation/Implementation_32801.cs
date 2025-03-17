4534ab02a89e6e123731dca87202992e55b1f8a2
sange17 <sange17@naver.com>
2025-03-17 22:33:23 +0900

    Add Implementation_32801.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_32801.cs
@@ -0,0 +1,25 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_32801
+    {
+        public void solve()
+        {
+            int[] inputs = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+
+            int count1 = 0;
+            int count2 = 0;
+            int count3 = 0;
+            for (int i = 1; i <= inputs[0]; i++)
+            {
+                if (i % inputs[1] == 0 && i % inputs[2] == 0)
+                    count3++;
+                else if (i % inputs[1] == 0)
+                    count1++;
+                else if (i % inputs[2] == 0)
+                    count2++;
+            }
+
+            Console.Write(count1 + " " + count2 + " " + count3);
+        }
+    }
+}
