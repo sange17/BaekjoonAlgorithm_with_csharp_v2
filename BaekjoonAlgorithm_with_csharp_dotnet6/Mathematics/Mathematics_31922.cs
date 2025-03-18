7b38dfc29acf16037ed8de1014cb7721fef6c43f
sange17 <sange17@naver.com>
2025-03-18 22:25:47 +0900

    Add Mathematics_31922.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Mathematics/Mathematics_31922.cs
@@ -0,0 +1,11 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Mathematics
+{
+    class Mathematics_31922
+    {
+        public void solve()
+        {
+            int[] array = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+            Console.Write(Math.Max(array[0] + array[2], array[1]));
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Implementation_32801 func = new Implementation_32801();
+            Mathematics_31922 func = new Mathematics_31922();
 
             func.solve();
         }
