4137b23a4c8be739f91079a29d4a0fc3e0df9c72
sange17 <sange17@naver.com>
2025-03-21 23:18:01 +0900

    Add Implementation_33571.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_33571.cs
@@ -0,0 +1,20 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_33571
+    {
+        public void solve()
+        {
+            char[] array = { 'A', 'a', 'b', 'D', 'd', 'e', 'g', 'O', 'o', 'P', 'p', 'Q', 'q', 'R', '@' };
+            string line = Console.ReadLine()!;
+            int count = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (array.Contains(line[i])) count++;
+                else if (line[i] == 'B') count += 2;
+            }
+
+            Console.Write(count);
+        }
+    }
+}
