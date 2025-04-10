8e8376fd511e957adbf3da4b2c850add013e7a33
sange17 <sange17@naver.com>
2025-04-10 20:54:33 +0900

    Add Implementation_05102.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_05102.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Channels;
+
+namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_05102
+    {
+        public void solve()
+        {
+            while (true)
+            {
+                string line = Console.ReadLine()!;
+                int A = Int32.Parse(line.Split(" ")[0]);
+                int B = Int32.Parse(line.Split(" ")[1]);
+                int diff = A - B;
+
+                if (A == 0 && B == 0) break;
+
+                if(diff > 3)
+                {
+                    Console.WriteLine(diff % 2 == 0 ? diff / 2 + " 0" : diff / 2 - 1 + " 1");
+                }
+                else if(diff == 3)
+                    Console.WriteLine("0 1");
+                else if (diff == 2) 
+                    Console.WriteLine("1 0");
+                else
+                    Console.WriteLine("0 0");
+            }
+        }
+    }
+}
