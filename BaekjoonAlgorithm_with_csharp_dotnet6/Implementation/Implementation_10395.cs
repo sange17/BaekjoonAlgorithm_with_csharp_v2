1efb35664ca6b1a3c8153bcbef90e73c7aa0ff48
sange17 <sange17@naver.com>
2025-04-23 12:16:33 +0900

    Add Implementation_10395.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_10395.cs
@@ -0,0 +1,26 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_10395
+    {
+        public void solve()
+        {
+            int[] bools1 = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+            int[] bools2 = Console.ReadLine()!.Split(" ").Select(int.Parse).ToArray();
+            bool check = true;
+
+            for (int i = 0; i < bools1.Length; i++)
+            {
+                if (bools1[i] == bools2[i])
+                {
+                    check = false;
+                    break;
+                }
+            }
+
+            if (!check)
+                Console.Write("N");
+            else
+                Console.Write("Y");
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Strings_09783 func = new Strings_09783();
+            Implementation_10395 func = new Implementation_10395();
 
             func.solve();
         }
