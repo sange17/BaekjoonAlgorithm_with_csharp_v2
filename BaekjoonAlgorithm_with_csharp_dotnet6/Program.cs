d44d527f5de2872a6189c0206f482916f52572a9
sange17 <sange17@naver.com>
2025-05-11 22:05:49 +0900

    Add Strings_31616.cs

--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_05292 func = new Mathematics_05292();
+            Strings_31616 func = new Strings_31616();
 
             func.solve();
         }
---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Strings/Strings_31616.cs
@@ -0,0 +1,15 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Strings
+{
+    class Strings_31616
+    {
+        public void solve()
+        {
+            int n = Int32.Parse(Console.ReadLine()!);
+            string line = Console.ReadLine()!;
+
+            line = string.Concat(line.Distinct());
+            if(line.Length == 1) Console.Write("Yes");
+            else Console.Write("No");
+        }
+    }
+}
