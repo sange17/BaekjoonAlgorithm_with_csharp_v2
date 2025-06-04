64cd3d240022ac5fbcf281b1c2ef3cdbc156e609
sange17 <sange17@naver.com>
2025-06-04 21:52:57 +0900

    Add Implementation_09517.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Implementation/Implementation_09517.cs
@@ -0,0 +1,28 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Implementation
+{
+    class Implementation_09517
+    {
+        public void solve()
+        {
+            int K = Int32.Parse(Console.ReadLine()!);
+            int N = Int32.Parse(Console.ReadLine()!);
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                string line = Console.ReadLine()!;
+                int T = Int32.Parse(line.Split(" ")[0]);
+                string Z = line.Split(" ")[1];
+
+                sum += T;
+                if (sum >= 210)
+                {
+                    Console.Write(K);
+                    break;
+                }
+
+                if (Z == "T") K++;
+                if (K > 8) K = 1;
+            }
+        }
+    }
+}
--- BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Program.cs
@@ -16,7 +16,7 @@ namespace BaekjoonAlgorithm_with_csharp_v2
     {
         static void Main(string[] args)
         {
-            Mathematics_15818 func = new Mathematics_15818();
+            Implementation_09517 func = new Implementation_09517();
 
             func.solve();
         }
