f2b5471c938700a02bd833773723e27b43956cb5
sange17 <sange17@naver.com>
2025-06-14 18:23:10 +0900

    Add Mathematics_18238.cs

---
+++ BaekjoonAlgorithm_with_csharp_dotnet6/Greedy/Mathematics_18238.cs
@@ -0,0 +1,21 @@
+﻿namespace BaekjoonAlgorithm_with_csharp_dotnet6.Greedy
+{
+    internal class Mathematics_18238
+    {
+        public void solve()
+        {
+            string input = Console.ReadLine()!;
+            char current = 'A';
+            int totalTime = 0;
+
+            foreach (char target in input)
+            {
+                int distance = Math.Abs(current - target);
+                totalTime += Math.Min(distance, 26 - distance);
+                current = target;
+            }
+
+            Console.Write(totalTime);
+        }
+    }
+}
